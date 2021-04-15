ba3614c38775a091509dc37d3447270253d37587
henrikmotzkus <henrik@motzkus.info>
2021-04-15 10:45:53 +0000

    chmod

---
+++ Linux/installscript.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# This configurates the linux typo3 machine
+
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt update
+sudo apt update
+sudo apt install nginx
+sudo systemctl stop nginx.service
+sudo systemctl start nginx.service
+sudo systemctl enable nginx.service
+
+sudo apt-get install php7.4-fpm php7.4-cli php7.4-mysql php7.4-gd php7.4-imagick php7.4-tidy php7.4-xmlrpc
+
+
+sudo cp /etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php.inibackup
+sudo cp php.ini /etc/php/7.4/fpm/
+
+
+sudo systemctl restart nginx.service
+sudo systemctl restart php7.4-fpm
+
+
+sudo cp default /etc/nginx/sites-enabled/
+
+sudo nginx -t
