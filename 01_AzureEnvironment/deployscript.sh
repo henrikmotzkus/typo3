73063209a2bbc2cbf2e81f7c3599e5879fc8849f
henrikmotzkus <henrik@motzkus.info>
2021-04-26 12:59:41 +0000

    installscript

--- 01_AzureEnvironment/deployscript.sh
+++ 01_AzureEnvironment/deployscript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RESOURCE_GROUP='typo3test2'
+RESOURCE_GROUP='typo3test4'
 LOCATION='westeurope'
 
 az login
@@ -12,7 +12,8 @@ az group create --name $RESOURCE_GROUP --location $LOCATION
 
 #Deploy VNET
 az deployment group create -n VNET -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_VNET/azuredeploy.json -p ./System/01_AzureEnvironment/01_VNET/azuredeploy.parameters.json
-az deployment group create -n VNET -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_INFRA/azuredeploy.json -p ./System/01_AzureEnvironment/01_INFRA/azuredeploy.parameters.json
+
+az deployment group create -n typo3 -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_INFRA/azuredeploy.json -p ./System/01_AzureEnvironment/01_INFRA/azuredeploy.parameters.json --parameters vmssName=$RESOURCE_GROUP
 
 
 az deployment group create -g ECE_WEB -t https://raw.githubusercontent.com/henrikmotzkus/typo3/main/ARM/azuredeploy.json?token=AFO2IHW7SQ3Z7OJY253WKJLAPBCJ2 -p https://raw.githubusercontent.com/henrikmotzkus/typo3/main/ARM/azuredeploy.parameters.json?token=AFO2IHTQJT5EMSVIGDZFL23APBCLE
