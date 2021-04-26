f6a68dec1ad5ac6c80434eb2fd89889fa73cf474
henrikmotzkus <henrik@motzkus.info>
2021-04-26 12:06:25 +0000

    first

---
+++ 01_AzureEnvironment/deployscript.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+RESOURCE_GROUP='typo3test2'
+LOCATION='westeurope'
+
+az login
+
+az account set --subscription "Microsoft Azure Internal Consumption"
+
+az group create --name $RESOURCE_GROUP --location $LOCATION
+
+
+#Deploy VNET
+az deployment group create -n VNET -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_VNET/azuredeploy.json -p ./System/01_AzureEnvironment/01_VNET/azuredeploy.parameters.json
+az deployment group create -n VNET -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_INFRA/azuredeploy.json -p ./System/01_AzureEnvironment/01_INFRA/azuredeploy.parameters.json
+
+
+az deployment group create -g ECE_WEB -t https://raw.githubusercontent.com/henrikmotzkus/typo3/main/ARM/azuredeploy.json?token=AFO2IHW7SQ3Z7OJY253WKJLAPBCJ2 -p https://raw.githubusercontent.com/henrikmotzkus/typo3/main/ARM/azuredeploy.parameters.json?token=AFO2IHTQJT5EMSVIGDZFL23APBCLE
+
+az deployment group create -g ECE_WEB -f azuredeploy.json -p azuredeploy.parameters.json
+
+
