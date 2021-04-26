28446eb5e5d6f476cddeb3d2a076679d84ed15a7
henrikmotzkus <henrik@motzkus.info>
2021-04-26 12:19:27 +0000

    files

--- 01_AzureEnvironment/01_INFRA/installscript.sh
+++ 01_AzureEnvironment/01_INFRA/installscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# This configurates the linux typo3 dev machine
+# This configurates the linuy typo3 worker node
 
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
@@ -16,13 +16,10 @@ sudo apt-get install php7.4-xml
 sudo apt-get install php7.4-zip
 sudo apt-get install php7.4-intl
 
-
-
-
+git clone https://github.com/henrikmotzkus/typo3.git
 
 sudo cp /etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php.inibackup
 sudo cp php.ini /etc/php/7.4/fpm/
-
 
 sudo systemctl restart nginx.service
 sudo systemctl restart php7.4-fpm
@@ -35,4 +32,4 @@ sudo nginx -t
 chmod -R a+rwx typo3/
 
 # Developer machine only 
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
