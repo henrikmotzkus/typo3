911531dcebaf1753086a26c97611dec35961618e
henrikmotzkus <henrik@motzkus.info>
2021-04-27 07:12:26 +0000

    arm und script

--- 01_AzureEnvironment/deployscript.sh
+++ 01_AzureEnvironment/deployscript.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-RESOURCE_GROUP='typo3test8'
+RESOURCE_GROUP='typo3test10'
 LOCATION='westeurope'
+az group create --name $RESOURCE_GROUP --location $LOCATION
+az deployment group create -n typo3 -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_INFRA/azuredeploy.json -p ./System/01_AzureEnvironment/01_INFRA/azuredeploy.parameters.json --parameters vmssName=$RESOURCE_GROUP
+
 
 az login
 
 az account set --subscription "Microsoft Azure Internal Consumption"
 
-az group create --name $RESOURCE_GROUP --location $LOCATION
+
 # Deploy INFRA
-az deployment group create -n typo3 -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_INFRA/azuredeploy.json -p ./System/01_AzureEnvironment/01_INFRA/azuredeploy.parameters.json --parameters vmssName=$RESOURCE_GROUP
+
 
 az deployment group create -g ECE_WEB -t https://raw.githubusercontent.com/henrikmotzkus/typo3/main/ARM/azuredeploy.json?token=AFO2IHW7SQ3Z7OJY253WKJLAPBCJ2 -p https://raw.githubusercontent.com/henrikmotzkus/typo3/main/ARM/azuredeploy.parameters.json?token=AFO2IHTQJT5EMSVIGDZFL23APBCLE
 
