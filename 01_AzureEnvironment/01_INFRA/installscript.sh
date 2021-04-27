e2f484831827445986aa355b42f1ad0cd886d108
henrikmotzkus <henrik@motzkus.info>
2021-04-27 07:30:36 +0000

    script

--- 01_AzureEnvironment/01_INFRA/installscript.sh
+++ 01_AzureEnvironment/01_INFRA/installscript.sh
@@ -6,7 +6,7 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 
 
-sudo apt-get install nginx
+sudo apt-get install nginx -y
 sudo systemctl stop nginx.service
 sudo systemctl start nginx.service
 sudo systemctl enable nginx.service
@@ -21,17 +21,19 @@ cd /home/adminlocal
 
 git clone https://github.com/henrikmotzkus/typo3.git
 
+chmod -R a+rwx typo3/
+
+
 sudo cp /etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php.inibackup
 sudo cp ./typo3/01_AzureEnvironment/01_INFRA/php.ini /etc/php/7.4/fpm/
-
-sudo systemctl restart nginx.service
-sudo systemctl restart php7.4-fpm
-
 sudo cp ./typo3/01_AzureEnvironment/01_INFRA/default /etc/nginx/sites-enabled/
 
 sudo nginx -t
 
-chmod -R a+rwx typo3/
+sudo systemctl restart nginx.service
+sudo systemctl restart php7.4-fpm
+
+
 
 # Developer machine only 
 # curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
