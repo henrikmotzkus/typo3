2f357e994d5d0d6f3a30ca65c2d151eabc41ed72
henrikmotzkus <henrik@motzkus.info>
2021-05-04 17:31:04 +0000

    pe nested

--- 01_AzureEnvironment/deployscript.sh
+++ 01_AzureEnvironment/deployscript.sh
@@ -7,7 +7,7 @@ az group create --name $RESOURCE_GROUP --location $LOCATION
 az deployment group create -n typo3 -g $RESOURCE_GROUP -f ./System/01_AzureEnvironment/01_INFRA/azuredeploy.json -p ./System/01_AzureEnvironment/01_INFRA/azuredeploy.parameters.json --parameters vmssName=$RESOURCE_GROUP
 
 az group delete --name typo3test20 --yes &
-az group delete --name typo3test22 --yes &
+az group delete --name typo3test23 --yes &>/dev/null & 
 
 
 
